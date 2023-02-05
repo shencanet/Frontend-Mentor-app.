e979f0094db9f2998fd448d7c76307c0a6ec1c73
shen <santi.dionis@gmail.com>
2023-02-05 13:46:47 +0100

    ADD: javascript

--- main.js
+++ main.js
@@ -2,13 +2,34 @@
 fetch("./data.json")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
-    let dailtBtn = document.querySelector("#daily");
+    
+    let dailytBtn = document.querySelector("#daily");
     let weeklyBtn = document.querySelector("#weekly");
     let monthlyBtn = document.querySelector("#monthly");
-    addEventListener('click', ()=> {
+    let seconsSection = document.querySelector('.second-section');
+    
+    dailytBtn.addEventListener('click', ()=> {
         data.forEach(element => {
             
-        });
-    });
-  });
+            console.log(element)
+            seconsSection.innerHTML +=`<div class="card">
+            <div class="card__background">
+              <img class="card__image" src="./images/icon-work.svg" alt="Work" />
+            </div>
+            <div class="card__details">
+              <div class="card__activity">
+                <p class="card__title">Work</p>
+                <img
+                  class="card_dots"
+                  src="./images/icon-ellipsis.svg"
+                  alt="Three dots"
+                />
+              </div>
+              <div class="card__time">
+                <p class="card__hour">5hrs</p>
+                <p class="card__previous">Previous - 7hrs</p>
+              </div>
+            </div>
+          </div>`;         });
+    });//cierre listener
+  });//cierre fetch
